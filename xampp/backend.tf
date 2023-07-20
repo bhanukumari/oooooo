fbc95330aa2509aa959784badf285502eeb91c4f
bhanukumari <117707378+bhanukumari@users.noreply.github.com>
2023-07-20 10:18:53 +0530

    Delete xampp directory

--- xampp/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "sprint3-snaatak"
-    key     = "Ot-micro/env/dev/databases/mysql/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- xampp/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "network_state" {
-  backend = "s3"
-  config = {
-    bucket = "sprint3-snaatak"
-    key    = "Ot-micro/env/dev/network/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
